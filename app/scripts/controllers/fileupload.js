733dcbb9102613e237dbda9f4b6895b4ccc5eafc
ArrogantBeggar <jorge.porras@cetys.edu.mx>
2016-10-10 19:35:13 -0700

    Finally achieved file upload pt 2

--- app/scripts/controllers/fileupload.js
+++ app/scripts/controllers/fileupload.js
@@ -1,4 +1,5 @@
-'use strict';
+var signedUrlEndpoint =
+  "https://d8mld28cl9.execute-api.us-east-1.amazonaws.com/dev/phiv-signedurl/";
 
 /**
  * @ngdoc function
@@ -8,10 +9,44 @@
  * Controller of the phylogeneticTreesApp
  */
 angular.module('phylogeneticTreesApp')
-  .controller('FileuploadCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('FileuploadCtrl', function ($scope) {
+
+    $scope.uploadedFile = function(element){
+      $scope.$apply(function($scope){
+        $scope.files = element.files;
+      })
+    };
+
+    $scope.addFile = function(){
+      $scope.uploadFile($scope.files)
+    };
+
+    $scope.uploadFile = function(files){
+      for(var i = 0; i < files.length; i++){
+        var formData = new FormData();
+        var file = files[i];
+        console.log(file);
+        var key = file.name;
+        var type = file.type.replace('/', '%2F');
+        formData.append(key, file);
+        $.ajax({
+          type: "GET",
+          url: signedUrlEndpoint + key + '/multipart%2Fform-data'
+        }).done(
+            function(getResponse){
+              console.log(getResponse);
+              $.ajax({
+                type: "PUT",
+                url: getResponse,
+                data: formData,
+                processData: false,
+                contentType: "multipart/form-data",
+                transformRequest: angular.identity
+              }).done(
+                  function(putResponse){
+                    console.log(putResponse);
+                  })
+            })
+      }
+    };
   });
